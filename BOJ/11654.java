8481ad27e9d49c1d178a5b721d267d88c9ff0087
jgchoiqa <jgchoi.qa@gmail.com>
2017-03-17 16:43:20 +0900

    boj 11654 add

---
+++ BOJ/11654.java
@@ -0,0 +1,15 @@
+package Joshua.PS.BOJ.main;
+
+import java.util.Scanner;
+
+/**
+ * Created by jgchoi.qa on 2017. 3. 17..
+ */
+public class ascii {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        System.out.println((int)input.charAt(0));
+    }
+}
