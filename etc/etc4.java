f8693f479aeca033f4652c2068df01ba30ab69c7
joshua <jgchoi.qa@gmail.com>
2017-06-18 15:33:19 +0900

    etc4 modify

--- etc/etc4.java
+++ etc/etc4.java
@@ -35,38 +35,33 @@ public class Main {
 
     // 원래 코드가 truck 68000, w 47520 input으로 0.98초 정도 걸렸는데 새로 구현한 코드는 0.45초 정도.
     // truck 136000, w 95040 input으로도 1.76초 걸리는걸 보면 진짜 빨라진건데 더 최적화 할 수 있는건지 알아봐야겠다.
+    // 한번 더 고친 코드에서는 같은 input으로 1.27~1.32초 걸린다. 세그먼트 트리를 쓰면 더 빨라질까...
 
     public static int[] solution(int[] truck, int[] w) {
-        long start = System.currentTimeMillis();
         int[] answer = new int[w.length];
-        boolean flag;
         Arrays.fill(answer, -1);
+        int first = 0;
 
         for(int i = 0; i < w.length; i++) {
-            flag = false;
-            for(int j = 0; j < truck.length; j++) {
-                if(truck[j] == 0) {
+            if(i > 0 && w[i] >= w[i-1]) {
+                if(answer[i-1] == -1) {
+                    answer[i] = -1;
                     continue;
                 }
-                if(i > 0 && !flag && w[i] >= w[i-1]) {
-                    if(answer[i-1] == -1) {
-                        answer[i] = -1;
-                        break;
-                    }
-                    j = answer[i-1] - 1;
-                    flag = true;
-                }
-                if(w[i] <= truck[j]) {
+                first = answer[i-1] - 1;
+            } else {
+                first = 0;
+            }
+            for(int j = first; j < truck.length; j++) {
+                if(truck[j] == 0 || w[i] > truck[j]) {
+                    continue;
+                } else if(w[i] <= truck[j]) {
                     truck[j] -= w[i];
                     answer[i] = j+1;
                     break;
                 }
             }
         }
-        long end = System.currentTimeMillis();
-
-        System.out.println( "실행 시간 : " + ( end - start )/1000.0 );
-
         return answer;
     }
 }
