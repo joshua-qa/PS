0223ec515777d4a6700b306706b43001669eaaad
jgchoiqa <jgchoi.qa@gmail.com>
2017-07-06 18:55:14 +0900

    boj 2965 add

---
+++ BOJ/2965.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Main().run();
+    }
+
+    public void run() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int z = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(y-x-1, z-y-1));
+    }
+}
