751f696d9a72ec03a41af3e995f967c204e3422e
joshua <jgchoi.qa@gmail.com>
2017-07-08 12:53:33 +0900

    boj 9655 add

---
+++ BOJ/9655.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Main().run();
+    }
+
+    public void run() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        if(n % 2 == 0) {
+            System.out.print("CY");
+        } else {
+            System.out.print("SK");
+        }
+    }
+}
