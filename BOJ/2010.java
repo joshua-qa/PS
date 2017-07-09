304d7662a695baa422950d45ecd4cbdbbfaa8aaf
joshua <jgchoi.qa@gmail.com>
2017-07-10 01:16:22 +0900

    boj 2010 add

---
+++ BOJ/2010.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Main().run();
+    }
+
+    public void run() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+        for(int i = 0; i < n; i++) {
+            result += Integer.parseInt(br.readLine()) - 1;
+        }
+
+        System.out.println(result+1);
+    }
+}
