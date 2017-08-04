e36f39a10002003a4c4cb7ba11f125566f9ff252
joshua <jgchoi.qa@gmail.com>
2017-08-04 15:28:45 +0900

    boj 5338 add

---
+++ BOJ/5338.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        new Main().run();
+    }
+
+    public void run() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        sb.append("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+        System.out.print(sb);
+    }
+}
