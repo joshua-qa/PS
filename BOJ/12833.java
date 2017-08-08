35804f7d29a25efc5670f5ab7fe78368a03ff6c7
joshua <jgchoi.qa@gmail.com>
2017-08-09 00:55:08 +0900

    boj 12833 add

---
+++ BOJ/12833.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new Main().run();
+    }
+
+    public void run() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(c % 2 == 0 ? a : a ^ b);
+    }
+}
