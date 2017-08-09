1ad23c911556656a31697bbd59553102a6bbfe81
joshua <jgchoi.qa@gmail.com>
2017-08-09 13:12:29 +0900

    boj 2914 add

---
+++ BOJ/2914.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    int n, m;
+    StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        new Main().run();
+    }
+
+    public void run() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int ans = n * (m-1) + 1;
+
+        bw.write(ans + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
