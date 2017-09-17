c00d85f063c25cdec74569cb8b34fa95e6d5565a
joshua <jgchoi.qa@gmail.com>
2017-09-18 02:16:33 +0900

    boj 1072 modify

--- BOJ/1072.java
+++ BOJ/1072.java
@@ -1,78 +1,41 @@
-package Joshua.PS.BOJ;
-
-import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        InputStream inputStream = System.in;
-        OutputStream outputStream = System.out;
-        InputReader in = new InputReader(inputStream);
-        PrintWriter out = new PrintWriter(outputStream);
-        Task task = new Task();
-        task.run(in, out);
-        out.close();
+        new Main().run();
     }
 
-    static class Task {
-        int ans, x, y;
-        double z;
-        StringBuilder sb = new StringBuilder();
-        public void run(InputReader in, PrintWriter out) {
-            String s;
-            while((s = in.next()) != null) {
-                x = Integer.parseInt(s);
-                y = in.nextInt();
-                z = (double)x / (double)y;
-                sb.append(search(1, y*2, (int)z+1) + "\n");
+    long x, y, z;
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()) {
+            x = sc.nextLong();
+            y = sc.nextLong();
+            z = (y * 100) / x;
+            if((int)z >= 99) {
+                System.out.println("-1");
+            } else {
+                System.out.println(search(1, 1000000000L));
             }
-
-            out.println(ans);
-        }
-
-        public int search(int start, int end, int target) {
-            int mid = 0;
-            while(start < end) {
-                mid = (start + end);
-
-                int newX = x + mid;
-                int newY = y + mid;
-                double newZ = (double) newX / (double) newY;
-
-                if (newZ < target) {
-                    start = mid+1;
-                } else {
-                    end = mid-1;
-                }
-            }
-
-            return mid;
         }
     }
 
-    static class InputReader {
-        public BufferedReader reader;
-        public StringTokenizer tokenizer;
+    public long search(long start, long end) {
+        long mid = 0;
+        while(start < end) {
+            mid = (start + end) / 2;
 
-        public InputReader(InputStream stream) {
-            reader = new BufferedReader(new InputStreamReader(stream), 32768);
-            tokenizer = null;
-        }
+            long newX = x + mid;
+            long newY = y + mid;
+            long newZ = (newY * 100) / newX;
 
-        public String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+            if (z == newZ) {
+                start = mid+1;
+            } else {
+                end = mid;
             }
-            return tokenizer.nextToken();
         }
 
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
+        return start;
     }
 }
