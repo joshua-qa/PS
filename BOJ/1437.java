c0a0446664afd2d1be3a8839e4adbb4c926640b1
jgchoiqa <jgchoi.qa@gmail.com>
2017-10-02 23:07:58 +0900

    boj 1437 add

---
+++ BOJ/1437.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
+    }
+
+    static class Task {
+        int n;
+        long ans;
+        final int MOD = 10007;
+        public void run(InputReader in, PrintWriter out) {
+            n = in.nextInt();
+
+            if(n != 0) {
+                ans = 1;
+            }
+            while(n > 0) {
+                if(n < 4) {
+                    ans *= n;
+                    n = 0;
+                } else if(n == 4) {
+                    ans *= 4;
+                    n = 0;
+                } else {
+                    n -= 3;
+                    ans *= 3;
+                }
+                ans %= MOD;
+            }
+
+            out.print(ans);
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public String nextLine() {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
