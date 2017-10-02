fad611ababf8b1fb8a139dd3d9fb5cd2892533ab
jgchoiqa <jgchoi.qa@gmail.com>
2017-10-02 20:40:29 +0900

    boj 1485 modify (마름모 예외케이스 추가)

--- BOJ/1485.java
+++ BOJ/1485.java
@@ -44,8 +44,10 @@ public class Main {
                 int length2 = (r.y - p.y) * (r.y - p.y) + (r.x - p.x) * (r.x - p.x);
                 int length3 = (s.y - r.y) * (s.y - r.y) + (s.x - r.x) * (s.x - r.x);
                 int length4 = (s.y - q.y) * (s.y - q.y) + (s.x - q.x) * (s.x - q.x);
+                int diag1 = (r.y - q.y) * (r.y - q.y) + (q.x - r.x) * (q.x - r.x);
+                int diag2 = (s.y - p.y) * (s.y - p.y) + (p.x - s.x) * (p.x - s.x);
 
-                if(length1 == length2 && length3 == length4 && length1 == length3) {
+                if(length1 == length2 && length3 == length4 && length1 == length3 && diag1 == diag2) {
                     sb.append("1\n");
                 } else {
                     sb.append("0\n");
