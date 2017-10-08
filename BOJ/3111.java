e887bdce8ac2d85c1c4eb3290182a43226af1cdd
joshua <jgchoi.qa@gmail.com>
2017-10-08 22:18:44 +0900

    boj 3111 add

---
+++ BOJ/3111.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
+    }
+
+    static class Task {
+        int n, flag = -1;
+        public void run(InputReader in, PrintWriter out) {
+            String a = in.next();
+            StringBuilder sb = new StringBuilder(in.next());
+            n = a.length();
+
+            while(true) {
+                int remove;
+                if(flag == -1) {
+                    remove = sb.indexOf(a);
+                } else {
+                    remove = sb.lastIndexOf(a);
+                }
+
+                if(remove < 0) {
+                    break;
+                }
+                sb.delete(remove, remove+n);
+                flag *= -1;
+            }
+
+            out.print(sb);
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
