616bf8a9f7ab052740f5bb3a1591efb0eca9a725
joshua <jgchoi.qa@gmail.com>
2017-10-09 14:47:04 +0900

    boj 14502 modify

--- BOJ/14502.java
+++ BOJ/14502.java
@@ -1,109 +1,141 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    int[] Dx = {-1, 0, 0, 1};
-    int[] Dy = {0, -1, 1, 0};
-    int[][] map;
-    boolean[][] check;
-    int n, m, available, max, res, currX, currY;
-    ArrayList<Point> virusList = new ArrayList<>();
-    StringTokenizer st;
-    public static void main(String[] args) throws IOException {
-        new Main().run();
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
     }
 
-    public void run() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static class Point {
+        int x;
+        int y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        max = n * m;
-        check = new boolean[n][m];
-        available = max - 3;
+    static class Task {
+        int[] Dx = {-1, 0, 0, 1};
+        int[] Dy = {0, -1, 1, 0};
+        int[][] map;
+        int n, m, ax, ay, bx, by, cx, cy, available, max, res, currX, currY;
+        Queue<Point> visit = new LinkedList<>();
+        ArrayList<Point> virusList = new ArrayList<>();
+        public void run(InputReader in, PrintWriter out) {
+            n = in.nextInt();
+            m = in.nextInt();
+            map = new int[n][m];
+            max = n * m;
+            available = max - 3;
 
-        for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] != 0) {
-                    available--;
-                    if(map[i][j] == 2) {
-                        virusList.add(new Point(i, j));
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < m; j++) {
+                    map[i][j] = in.nextInt();
+                    if(map[i][j] != 0) {
+                        available--;
+                        if(map[i][j] == 2) {
+                            virusList.add(new Point(i, j));
+                        }
                     }
                 }
             }
-        }
 
-        for(int i = 0; i < max - 2; i++) {
-            if(map[i / m][i % m] != 0) {
-                continue;
-            }
-            map[i / m][i % m] = 1;
-            for(int j = i+1; j < max - 1; j++) {
-                if(map[j / m][j % m] != 0) {
+            for(int i = 0; i < max - 2; i++) {
+                ax = i / m;
+                ay = i % m;
+                if(map[ax][ay] != 0) {
                     continue;
                 }
-                map[j / m][j % m] = 1;
-                for(int k = j+1; k < max; k++) {
-                    if(map[k / m][k % m] != 0) {
+                map[ax][ay] = 1;
+                for(int j = i+1; j < max - 1; j++) {
+                    bx = j / m;
+                    by = j % m;
+                    if(map[bx][by] != 0) {
                         continue;
                     }
-                    map[k / m][k % m] = 1;
-                    int curr = bfs();
-                    if(curr > res) {
-                        res = curr;
+                    map[bx][by] = 1;
+                    for(int k = j+1; k < max; k++) {
+                        cx = k / m;
+                        cy = k % m;
+                        if(map[cx][cy] != 0) {
+                            continue;
+                        }
+                        map[cx][cy] = 1;
+                        int curr = bfs();
+                        if(curr > res) {
+                            res = curr;
+                        }
+                        map[cx][cy] = 0;
                     }
-                    map[k / m][k % m] = 0;
+                    map[bx][by] = 0;
                 }
-                map[j / m][j % m] = 0;
+                map[ax][ay] = 0;
             }
-            map[i / m][i % m] = 0;
+
+            out.print(res);
         }
 
-        System.out.print(res);
-    }
+        private int bfs() {
+            boolean[][] check = new boolean[n][m];
+            int ans = 0;
+            for(Point p : virusList) {
+                visit.add(p);
+                check[p.x][p.y] = true;
+            }
 
-    private int bfs() {
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(check[i], false);
-        }
-        int ans = 0;
-        Queue<Point> visit = new LinkedList<>();
-        for(Point p : virusList) {
-            visit.add(p);
-            check[p.x][p.y] = true;
-        }
+            while(!visit.isEmpty()) {
+                Point p = visit.remove();
 
-        while(!visit.isEmpty()) {
-            Point p = visit.remove();
-
-            for(int i = 0; i < Dx.length; i++) {
-                currX = p.x + Dx[i];
-                currY = p.y + Dy[i];
-                if(currX < 0 || currX >= n || currY < 0 || currY >= m) {
-                    continue;
-                }
-                if(!check[currX][currY] && map[currX][currY] == 0) {
-                    check[currX][currY] = true;
-                    ans++;
-                    visit.add(new Point(currX, currY));
+                for(int i = 0; i < Dx.length; i++) {
+                    currX = p.x + Dx[i];
+                    currY = p.y + Dy[i];
+                    if(currX < 0 || currX >= n || currY < 0 || currY >= m) {
+                        continue;
+                    }
+                    if(!check[currX][currY] && map[currX][currY] == 0) {
+                        check[currX][currY] = true;
+                        ans++;
+                        visit.add(new Point(currX, currY));
+                    }
                 }
             }
+            return available - ans;
         }
-        return available - ans;
     }
-}
 
-class Point {
-    int x;
-    int y;
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
     }
 }
