c9f318aaf0578f734ddadaa26f9cd6ff79f91be0
joshua <jgchoi.qa@gmail.com>
2017-10-24 13:02:12 +0900

    boj 3053 add

---
+++ BOJ/3053.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
+    }
+
+    static class Task {
+        int n;
+        double euclid, taxi;
+        final double PI = 3.14159265358979323;
+        public void run(InputReader in, PrintWriter out) {
+            n = in.nextInt();
+            
+            euclid = (double)n * (double)n * PI;
+            taxi = (double)n * (double)n * 2;
+
+            System.out.printf("%.6f\n", euclid);
+            System.out.printf("%.6f", taxi);
+        }
+
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
