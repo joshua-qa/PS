ac27ac06fa0a93494e53b0adb082e87ff217b427
joshua <jgchoi.qa@gmail.com>
2017-10-31 12:37:51 +0900

    boj 2845 add

---
+++ BOJ/2000/2845.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
+    }
+
+    static class Task {
+        int n, m, people, k;
+        StringBuilder sb = new StringBuilder();
+        public void run(InputReader in, PrintWriter out) {
+            n = in.nextInt();
+            m = in.nextInt();
+            people = n * m;
+
+            for(int i = 0; i < 5; i++) {
+                k = in.nextInt();
+                sb.append(k - people).append(" ");
+            }
+
+            out.print(sb);
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
