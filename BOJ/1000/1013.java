9b18e457fab5cd04ebaa4eab95f023c2d70458f7
joshua <jgchoi.qa@gmail.com>
2017-11-22 14:37:11 +0900

    boj 1013 add

---
+++ BOJ/1000/1013.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
+    }
+
+    static class Task {
+        int n;
+        final String REGEX = "(100+1+|01)+";
+        StringBuilder sb = new StringBuilder();
+        public void run(InputReader in, PrintWriter out) {
+            n = in.nextInt();
+
+            while(n-- > 0) {
+                String input = in.next();
+
+                sb.append(input.matches(REGEX) ? "YES" : "NO").append("\n");
+            }
+
+            out.print(sb);
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String nextLine() {
+            String s;
+            try {
+                s = reader.readLine();
+            } catch (Exception e) {
+                return "-1";
+            }
+            if(s == null || s.equals("")) {
+                return "-1";
+            }
+
+            return s;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
