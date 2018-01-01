532022712136fc70f8b528be8ffe11a9acd59c36
joshua.qa <jgchoi.qa@gmail.com>
2018-01-01 19:35:13 +0900

    boj 1225 add

---
+++ BOJ/1000/1225.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
+    }
+
+    static class Task {
+        public void run(InputReader in, PrintWriter out) {
+            char[] a = in.next().toCharArray();
+            char[] b = in.next().toCharArray();
+
+            long total = 0;
+
+            for(char ca : a) {
+                for(char cb : b) {
+                    total += (ca - '0') * (cb - '0');
+                }
+            }
+
+            out.print(total);
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String nextLine() {
+            String s;
+            try {
+                s = reader.readLine();
+            } catch (Exception e) {
+                return "-1";
+            }
+            if(s == null || s.equals("")) {
+                return "-1";
+            }
+
+            return s;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
