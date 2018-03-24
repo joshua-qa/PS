4cb4bfb4edc5b5ba917d6cc3ecd6724a7833d64e
joshua-qa <jgchoi.qa@gmail.com>
2018-03-24 20:10:55 +0900

    boj 1806 수정 (재채점해서 틀린거 다시 풀기)

--- BOJ/1000/1806.java
+++ BOJ/1000/1806.java
@@ -1,42 +1,89 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
+    }
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
-        int[] numArray = new int[n];
+    static class Task {
+        int n, m, s, e, sum, min;
+        int[] nums;
+        public void run(InputReader in, PrintWriter out) {
+            n = in.nextInt();
+            m = in.nextInt();
+            nums = new int[n];
 
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
-            numArray[i] = Integer.parseInt(st.nextToken());
-        }
-        int start = 0, end = 1, curr = numArray[start] + numArray[end], minSize = Integer.MAX_VALUE;
+            for(int i = 0; i < n; i++) {
+                nums[i] = in.nextInt();
+            }
 
-        while(end != n) {
-            if(curr >= s) {
-                minSize = Math.min(minSize, (end-start+1));
-                curr -= numArray[start++];
-                if (end + 1 == n) {
-                    break;
-                }
-                if(end == start) {
-                    curr += numArray[++end];
-                }
-            } else {
-                if (end + 1 == n) {
-                    break;
+            min = Integer.MAX_VALUE;
+            s = 0;
+            e = 1;
+            sum = nums[0];
+            if(sum >= m) {
+                min = 1;
+            }
+
+            while(e < n || s < n) {
+                if(sum < m && e < n) {
+                    sum += nums[e];
+                    e++;
                 } else {
-                    curr += numArray[++end];
+                    sum -= nums[s];
+                    s++;
                 }
+
+                if(sum >= m && min > (e-s)) {
+                    min = e-s;
+                }
+            }
+
+            out.print(min == Integer.MAX_VALUE ? 0 : min);
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public String nextLine() {
+            try {
+                return reader.readLine();
+            } catch (Exception e) {
+                return "";
             }
         }
 
-        System.out.println(minSize == Integer.MAX_VALUE ? 0 : minSize);
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
     }
 }
