4150e5f6b17c8ee60499f57bb9e2873f609cead8
joshua-qa <jgchoi.qa@gmail.com>
2020-01-04 21:03:58 +0900

    modify BOJ 1325 - 재채점 시간 초과로 다시 풀었으나 만족스럽지 않아 추후 다른 방법으로 다시 풀어볼 필요가 있음

--- BOJ/1000/1325.java
+++ BOJ/1000/1325.java
@@ -1,73 +1,105 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    private ArrayList<Integer>[] graph;
-    private ArrayList<Integer> maxList = new ArrayList<>();
-    private boolean[] check;
-    private int a, b, maxCount, n, count;
-    public static void main(String[] args) throws IOException {
-        new Main().run();
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
     }
 
-    public void run() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        StringBuffer sb = new StringBuffer();
-        int t;
+    static class Task {
+        int n, m, x, y, currMax = -1;
+        ArrayList<Integer>[] graph;
+        boolean[] visit;
+        int[] counts;
+        public void run(InputReader in, PrintWriter out) {
+            n = in.nextInt();
+            m = in.nextInt();
+            counts = new int[n+1];
+            graph = new ArrayList[n+1];
+            for (int i = 1; i <= n; i++) {
+                graph[i] = new ArrayList<>();
+            }
+            for (int i = 0; i < m; i++) {
+                x = in.nextInt();
+                y = in.nextInt();
+                graph[x].add(y);
+            }
+            for (int i = 1; i <= n; i++) {
+                visit = new boolean[n+1];
+                bfs(i);
+            }
 
-        check = new boolean[n+1];
-        graph = (ArrayList<Integer>[]) new ArrayList[n+1];
-        for(int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                if (currMax < counts[i]) {
+                    currMax = counts[i];
+                }
+            }
+
+            for (int i = 1; i <= n; i++) {
+                if (currMax == counts[i]) {
+                    out.write(i + " ");
+                }
+            }
+            out.flush();
         }
 
-        for(int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            graph[b].add(a);
+        private void bfs(int node) {
+            Queue<Integer> queue = new LinkedList<>();
+            queue.offer(node);
+            visit[node] = true;
+            while (!queue.isEmpty()) {
+                int curr = queue.poll();
+                for (int i : graph[curr]) {
+                    if (!visit[i]) {
+                        visit[i] = true;
+                        counts[i]++;
+                        queue.offer(i);
+                    }
+                }
+            }
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
         }
 
-        for(int i = 1; i <= n; i++) {
-            count = 0;
-            Arrays.fill(check, false);
-            t = dfs(i);
-            if(t > maxCount) {
-                maxCount = t;
-                maxList.clear();
-                maxList.add(i);
-            } else if(t == maxCount) {
-                maxList.add(i);
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public String nextLine() {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
 
-        for(int i : maxList) {
-            sb.append(i + " ");
+        public int nextInt() {
+            return Integer.parseInt(next());
         }
 
-        System.out.print(sb.toString());
-    }
-
-    private int dfs(int n) {
-        if(check[n]) {
-            return 0;
+        public long nextLong() {
+            return Long.parseLong(next());
         }
-
-        check[n] = true;
-        count++;
-
-        for(int i = 0; i < graph[n].size(); i++) {
-            int t = graph[n].get(i);
-            if(!check[t]) {
-                dfs(t);
-            }
-        }
-
-        return count;
     }
 }
