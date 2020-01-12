a5271a8ed0de84b2fa80fda987cb5e0f304e017b
joshua-qa <jgchoi.qa@gmail.com>
2020-01-12 17:58:40 +0900

    add BOJ 1629

---
+++ BOJ/1000/1629.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task task = new Task();
+        task.run(in, out);
+        out.close();
+    }
+
+    static class Task {
+        long n, m, mod;
+        public void run(InputReader in, PrintWriter out) {
+            n = in.nextLong();
+            m = in.nextLong();
+            mod = in.nextLong();
+            out.print(solve(n, m));
+        }
+
+        private long solve(long num, long pow) {
+            if (pow == 1) {
+                return num % mod;
+            }
+            long result = solve(num, pow/2) % mod;
+            return (pow & 1) == 1 ? ((result * result) % mod) * num % mod : (result * result) % mod;
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public String nextLine() {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
