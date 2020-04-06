8ef7ce3e7a3cb9759b435ce1b2b0085cd95d7bef
joshua-qa <jgchoi.qa@gmail.com>
2020-04-07 00:42:58 +0900

    Leetcode - Group Anagrams 처음 풀었을 때 코드를 보충

--- leetcode/GroupAnagrams.java
+++ leetcode/GroupAnagrams.java
@@ -11,6 +11,9 @@ public class Main {
         System.out.println(groupAnagrams(strs).toString());
     }
 
+    /**
+     * 풀고나서 솔루션을 보고 미흡한 부분만 개선해본 것. 10ms가 나왔다.
+     */
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> anagramListMap = new HashMap<>();
         for (String str : strs) {
@@ -35,4 +38,24 @@ public class Main {
         }
         return sb.toString();
     }
+
+    /**
+     * 처음 풀었을 때 코드. 솔루션을 올바르게 생각해낸 것은 좋으나, 부수적인 처리가 미흡하여 32ms가 나왔다..
+     */
+    public List<List<String>> groupAnagrams_origin(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> anagramListMap = new HashMap<>();
+        for (String str : strs) {
+            String anagramInfo = getAnagramInfo(str.toCharArray());
+
+            List<String> temp = anagramListMap.containsKey(anagramInfo) ? anagramListMap.get(anagramInfo) : new ArrayList<>();
+            temp.add(str);
+            anagramListMap.put(anagramInfo, temp);
+        }
+        for (String key : anagramListMap.keySet()) {
+            result.add(anagramListMap.get(key));
+        }
+        return result;
+    }
+
 }
