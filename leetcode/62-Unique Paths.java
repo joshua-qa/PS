55b9077b779a389a3b6cbd5d81b68b6044ea23be
joshua-qa <jgchoi.qa@gmail.com>
2022-08-01 21:12:44 +0900

    62. Unique Paths 풀이

---
+++ leetcode/62-Unique Paths.java
@@ -0,0 +1,17 @@
+// 1ms. 규칙을 잘 살펴보는 것이 도움된다.
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        Arrays.fill(dp[0], 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
