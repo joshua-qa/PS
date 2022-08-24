3d025cf8276c534423f757bc4fe2a493b580fc2f
joshua-qa <jgchoi.qa@gmail.com>
2022-08-24 23:56:47 +0900

    326. Power of Three 풀이

---
+++ leetcode/326-Power of Three.java
@@ -0,0 +1,12 @@
+// 21ms.
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n == 1) {
+            return true;
+        }
+        if (n <= 0 || n % 3 != 0) {
+            return false;
+        }
+        return isPowerOfThree(n / 3);
+    }
+}
