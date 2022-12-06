293f2f75271549a156d7ec2d7a29e736b905603f
joshua-qa <jgchoi.qa@gmail.com>
2022-12-06 20:53:44 +0900

    328. Odd Even Linked List 풀이

---
+++ leetcode/328-Odd Even Linked List.java
@@ -0,0 +1,31 @@
+// 0ms. 짝수 노드의 값들을 뒤로 일일히 밀어내는 방식으로 풀어도 20ms 정도로 통과는 하더라.
+// 링크드 리스트 문제는 아무리 많이 풀어도 부족한거 같다..
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode odd = head;
+        ListNode evenHead = head.next;
+        ListNode even = evenHead;
+        while (odd.next != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+
+        return head;
+    }
+}
